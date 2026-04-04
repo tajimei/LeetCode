2acddd61cc5d9023ed6b90d525cf57fa4fab652a
tajimei <g2560214@edu.cc.ocha.ac.jp>
2026-04-04 15:44:02 +0900

    Time: 11 ms (55.78%), Space: 12.3 MB (70.1%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,5 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        s = str(x)
-        return s == s[::-1]
+        # s = str(x)
+        # return s == s[::-1]
+
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_num = 0
+        original = x
+        while x > 0:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+        return original == reversed_num
