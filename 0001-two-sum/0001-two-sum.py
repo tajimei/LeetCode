32787f03d17937af45004cd6ef85bd12baa069b4
tajimei <g2560214@edu.cc.ocha.ac.jp>
2026-04-04 15:12:07 +0900

    Time: 1 ms (61.64%), Space: 12.9 MB (88.05%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        prev_map = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prev_map:
+                return [prev_map[diff], i]
+            prev_map[n] = i  
+        
