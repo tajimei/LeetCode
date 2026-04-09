62c9cd1da3f131871a33acecbc3cda060a26824b
tajimei <g2560214@edu.cc.ocha.ac.jp>
2026-04-09 13:52:58 +0900

    Time: 4 ms (42.64%), Space: 12.4 MB (97.45%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        mapping = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+        for char in s:
+            if char in mapping:
+                if stack:
+                    top_element = stack.pop()
+                else:
+                    top_element = '#'
+                if mapping[char] != top_element:
+                    return False
+
+            else:
+                stack.append(char)
+        return not stack
+        
